63e31b0a382cb21fc5ca11ef2f791ec517de63b0
Dima2430 <dima.volkkom@gmail.com>
2025-01-10 13:48:39 +0100

    Create Convert.java

---
+++ Task02/Convert.java
@@ -0,0 +1,16 @@
+public class Convert {
+    int minutesToConvert;
+
+    public Convert(int minutesToConvert) {
+        this.minutesToConvert = minutesToConvert;
+    }
+
+    public int ConvertToSec() {
+        return minutesToConvert * 60;
+    }
+
+    public static void main(String[] args) {
+    Convert seconds = new Convert(98);
+    System.out.println(seconds.ConvertToSec());
+}
+}
