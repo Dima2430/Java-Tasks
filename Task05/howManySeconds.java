a31d9aa68b5d09909078036ae7523d9f832da916
Dima2430 <dima.volkkom@gmail.com>
2025-01-13 16:01:35 +0100

    Create howManySeconds.java

---
+++ Task05/howManySeconds.java
@@ -0,0 +1,18 @@
+package Task05;
+import java.util.Scanner;
+public class howManySeconds {
+   
+
+    public static int convertHoursToSeconds(int hours) {
+        return hours * 60 * 60;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input the hours:");
+        int inputHours = scanner.nextInt();
+        System.out.println("Your hours converted to seconds: "+ howManySeconds.convertHoursToSeconds(inputHours));
+         scanner.close();
+}
+   
+}
