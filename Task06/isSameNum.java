0b281564e62244d52fe572bf256d3b8e12db9801
Dima2430 <dima.volkkom@gmail.com>
2025-01-14 20:48:56 +0100

    Create isSameNum.java

---
+++ Task06/isSameNum.java
@@ -0,0 +1,11 @@
+package Task06;
+
+public class isSameNum {
+    public static boolean checkIfSameNum(int num1, int num2) {
+        return num1 == num2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isSameNum.checkIfSameNum( 1, 0));
+    }
+}
