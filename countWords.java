cb0a5aa7c9eb880b93fc9a4917d9420132a33540
Dima2430 <142937464+Dima2430@users.noreply.github.com>
2025-01-25 15:31:52 +0100

    Create countWords.java

---
+++ countWords.java
@@ -0,0 +1,8 @@
+class countWords {
+public static int countF(String str) {
+return str.split('').length;
+}
+public static void main(String[] args) {
+System.out.println(countWords.countF("Just an example here move along"));
+}
+}
