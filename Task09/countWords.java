a7ddfa735f2301d57d01abe23d3c6f8d2a5142af
Dima2430 <dima.volkkom@gmail.com>
2025-01-27 23:19:01 +0100

    commit

--- Task09/countWords.java
+++ Task09/countWords.java
@@ -1,6 +1,6 @@
 class countWords {
 public static int countF(String str) {
-return str.split('').length;
+return str.split(" ").length;
 }
 public static void main(String[] args) {
 System.out.println(countWords.countF("Just an example here move along"));
---
+++ Task11/triangle.java
@@ -0,0 +1,10 @@
+package Task11;
+
+public class triangle {
+    public static double dots(int dots) {
+        return (dots * (dots + 1)) / 2;
+    }
+    public static void main(String[] args) {
+        System.out.println(dots(215));
+    }
+}
