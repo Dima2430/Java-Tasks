f5754ac1bd0db5280ec896c8f4205bf28d0f9842
Dima2430 <dima.volkkom@gmail.com>
2025-01-30 22:48:25 +0100

    Create nameShuffle.java

---
+++ Task14/nameShuffle.java
@@ -0,0 +1,15 @@
+package Task14;
+import java.util.Scanner;
+
+public class nameShuffle {
+    public static String shuffleNames(String names) {
+        String[] arr = names.split(" ");
+        return arr[1] + " " + arr[0];
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(shuffleNames(input));
+scanner.close();
+    }
+}
