c4bbd00c7927adb445ef6a8255a2fe07a053103d
Dima2430 <dima.volkkom@gmail.com>
2025-01-31 22:33:10 +0100

    Create discount.java

---
+++ Task15/discount.java
@@ -0,0 +1,10 @@
+package Task15;
+
+public class discount {
+    public static double priceWithDiscount(int price, int discount) {
+        return price - (price * discount / 100);
+    }
+    public static void main(String[] args) {
+        System.out.println(priceWithDiscount(100, 10));
+    }
+}
