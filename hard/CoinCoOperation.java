4288be5b4736acc1c737257af2d33f1940fd6269
Dima2430 <dima.volkkom@gmail.com>
2025-03-26 23:30:50 +0100

    Update CoinCoOperation.java

--- hard/CoinCoOperation.java
+++ hard/CoinCoOperation.java
@@ -1,6 +1,21 @@
 public class CoinCoOperation {
-    public static int[] getCoin(String[] firstP,String[] secondP) {
-       int first = 0, second = 0, count = 0;
-           
+    public static String getCoin(String[] firstP, String[] secondP) {
+        int first = 3, second = 3;
+        for (String string : firstP) {
+            if (string.equals("share")) {
+                first--;
+                second += 3;
+            }
+        }
+        for (String string : secondP) {
+            if (string.equals("share")) {
+                second--;
+                first += 3;
+            }
+        }
+        return "" + first + " " + second;
+    }
+    public static void main(String[] args) {
+        System.out.println(getCoin(new String[] { "steal", "steal", "steal" }, new String[] { "steal", "share", "share" }));
     }
 }
