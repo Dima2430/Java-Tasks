0c8a218370f0b92805e1d8b2f03f7cb53391b39c
Dima2430 <dima.volkkom@gmail.com>
2025-03-30 22:16:47 +0200

    Create PalindromeWord.java

---
+++ hard/PalindromeWord.java
@@ -0,0 +1,10 @@
+public class PalindromeWord {
+    public static boolean isPalindrome(String word) {
+        
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("racecar"));
+        System.out.println(isPalindrome("hello"));
+         System.out.println(isPalindrome("madam"));
+    }
+}
