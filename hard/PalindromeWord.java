be5c8b8f61e18c8f137f2f1a1759367fa8adcaba
Dima2430 <dima.volkkom@gmail.com>
2025-03-31 15:30:57 +0200

    Update PalindromeWord.java

--- hard/PalindromeWord.java
+++ hard/PalindromeWord.java
@@ -1,10 +1,19 @@
 public class PalindromeWord {
     public static boolean isPalindrome(String word) {
-        
+        String cleanWord = word.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        if (cleanWord.isEmpty() || cleanWord.length() <= 1) {
+            return true;
+        }
+        if (cleanWord.charAt(0) != cleanWord.charAt(cleanWord.length() - 1)) {
+            return false;
+        }
+        return isPalindrome(cleanWord.substring(1, cleanWord.length() - 1));
     }
     public static void main(String[] args) {
         System.out.println(isPalindrome("racecar"));
         System.out.println(isPalindrome("hello"));
-         System.out.println(isPalindrome("madam"));
+        System.out.println(isPalindrome("madam"));
+        System.out.println(isPalindrome("A man, a plan, a canal, Panama"));
     }
 }
