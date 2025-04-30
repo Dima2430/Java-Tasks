379edfafdb619ac3be77f3927a85e111f9f4a250
Dima2430 <dima.volkkom@gmail.com>
2025-04-30 13:37:13 +0200

    Update PrimeNumberInRange.java

--- medium/PrimeNumberInRange.java
+++ medium/PrimeNumberInRange.java
@@ -1,28 +1,33 @@
 package medium;
+
 public class PrimeNumberInRange {
+
     public static boolean hasPrimeInRange(int start, int end) {
         for (int i = start; i <= end; i++) {
-            if (isPrime(i))
-                return true;
+            if (isPrime(i)) return true;
         }
         return false;
     }
 
     public static boolean isPrime(int n) {
-        if (n == 2 || n == 3) {
-            return true;
-        }
-           if (n <= 1 || n % 2 == 0 || n % 3 == 0) {
-            return false;
-        }
-        for (int i = 5; i * i <= n; i+=6) {
-            if (n % i == 0 || n % (i + 2) == 0) {
-                return false;
-            }
+        if (n <= 1) return false;
+        if (n == 2 || n == 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) return false;
         }
         return true;
- }
+    }
+
     public static void main(String[] args) {
-        System.out.println(hasPrimeInRange(10, 13));
+        int count = 0;
+        while (count < 10) {
+            int rand = (int)(Math.random() * 99_000_001) + 1_000_000;
+            if (isPrime(rand)) {
+                System.out.println(rand + " is prime.");
+                count++;
+            }
+        }
     }
 }
